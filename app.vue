535f3871c54797e6fc4e9ce0d4db1d61fad58b80
Maikel Carballo <mcarballo@tutanota.com>
2022-09-04 02:08:02 -0400

    Update meta tags

--- app.vue
+++ app.vue
@@ -11,34 +11,29 @@ useHead({
       href: '/favicon.ico',
     },
   ],
-  meta: [
-    { content: 'Desarrollador Web Full Stack', name: 'description' },
-    {
-      content: 'programador web, desarrollador web, full stack',
-      name: 'keywords',
-    },
-    { content: 'Desarrollador Web Full Stack', property: 'og:title' },
-    { content: 'website', property: 'og:type' },
-    {
-      content: 'https://kimael-code.github.io/',
-      property: 'og:url',
-    },
-    { content: 'Maikel Carballo - Portafolio', property: 'og:site_name' },
-    { content: 'Desarrollador Web Full Stack', property: 'og:description' },
-    { content: 'summary_large_image', name: 'twitter:card' },
-    { content: '@_kimael_', name: 'twitter:site' },
-    { content: 'twitter.com/_kimael_', name: 'twitter:domain' },
-    { content: 'Desarrollador Web Full Stack', name: 'twitter:title' },
-    {
-      content: 'Desarrollador Web Full Stack. Profesor en Informática.',
-      name: 'twitter:description',
-    },
-  ],
 })
 </script>
 
 <template>
   <div>
+    <Head>
+      <Meta name="description" content="Sitio Web Personal de Maikel Carballo" />
+      <Meta name="author" content="Maikel Carballo" />
+      <Meta
+        name="keywords"
+        content="programador web, desarrollador web, full stack, portafolio, portfolio"
+      />
+      <Meta property="og:type" content="website" />
+      <Meta property="og:title" content="Sitio Web Personal de Maikel Carballo" />
+      <Meta property="og:url" content="https://kimael-code.github.io" />
+      <Meta property="og:site_name" content="Maikel Carballo" />
+      <Meta property="og:description" content="Sitio Web Personal de Maikel Carballo" />
+      <Meta property="og:image" content="https://twitter.com/_kimael_/photo" />
+      <Meta name="twitter:card" content="summary_large_image" />
+      <Meta name="twitter:site" content="@_kimael_" />
+      <Meta name="twitter:domain" content="twitter.com/_kimael_" />
+      <Meta name="twitter:title" content="Desarrollador Web Full Stack" />
+    </Head>
     <NuxtLayout>
       <NuxtLoadingIndicator />
       <NuxtPage />
