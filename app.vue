cc81a546fcc88984f6d77e6e93828aa21f497695
Maikel Carballo <mcarballo@tutanota.com>
2022-09-18 16:45:16 -0400

    ci(seo): remove seo meta tags

--- app.vue
+++ app.vue
@@ -9,67 +9,67 @@ useHead({
       href: '/favicon.ico',
     },
   ],
-  meta: [
-    {
-      property: 'og:type',
-      content: 'website',
-    },
-    {
-      property: 'og:title',
-      content: 'Maikel Carballo',
-    },
-    {
-      property: 'og:url',
-      content: 'https://kimael-code.github.io/',
-    },
-    {
-      property: 'og:image',
-      content:
-        'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
-    },
-    {
-      property: 'og:description',
-      content: 'Desarrollador Web Full Stack',
-    },
-    {
-      name: 'twitter:card',
-      content: 'summary_large_image',
-    },
-    {
-      name: 'twitter:title',
-      content: 'Maikel Carballo',
-    },
-    {
-      name: 'twitter:site',
-      content: 'https://kimael-code.github.io/',
-    },
-    {
-      name: 'twitter:creator',
-      content: '@_kimael_',
-    },
-    {
-      name: 'twitter:image',
-      content:
-        'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
-    },
-    {
-      name: 'twitter:description',
-      content: 'Desarrollador Web Full Stack',
-    },
-    {
-      name: 'description',
-      content: 'Sitio Web Personal de Maikel Carballo',
-    },
-    {
-      name: 'author',
-      content: 'Maikel Carballo',
-    },
-    {
-      name: 'keywords',
-      content:
-        'programador web, desarrollador web, full stack, portafolio, portfolio',
-    },
-  ],
+  // meta: [
+  //   {
+  //     property: 'og:type',
+  //     content: 'website',
+  //   },
+  //   {
+  //     property: 'og:title',
+  //     content: 'Maikel Carballo',
+  //   },
+  //   {
+  //     property: 'og:url',
+  //     content: 'https://kimael-code.github.io/',
+  //   },
+  //   {
+  //     property: 'og:image',
+  //     content:
+  //       'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
+  //   },
+  //   {
+  //     property: 'og:description',
+  //     content: 'Desarrollador Web Full Stack',
+  //   },
+  //   {
+  //     name: 'twitter:card',
+  //     content: 'summary_large_image',
+  //   },
+  //   {
+  //     name: 'twitter:title',
+  //     content: 'Maikel Carballo',
+  //   },
+  //   {
+  //     name: 'twitter:site',
+  //     content: 'https://kimael-code.github.io/',
+  //   },
+  //   {
+  //     name: 'twitter:creator',
+  //     content: '@_kimael_',
+  //   },
+  //   {
+  //     name: 'twitter:image',
+  //     content:
+  //       'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
+  //   },
+  //   {
+  //     name: 'twitter:description',
+  //     content: 'Desarrollador Web Full Stack',
+  //   },
+  //   {
+  //     name: 'description',
+  //     content: 'Sitio Web Personal de Maikel Carballo',
+  //   },
+  //   {
+  //     name: 'author',
+  //     content: 'Maikel Carballo',
+  //   },
+  //   {
+  //     name: 'keywords',
+  //     content:
+  //       'programador web, desarrollador web, full stack, portafolio, portfolio',
+  //   },
+  // ],
 })
 </script>
 
