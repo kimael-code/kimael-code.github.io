d088a77ee46db0cff3da3f53848e782bd42f76b1
Maikel Carballo <mcarballo@tutanota.com>
2022-10-07 22:34:57 -0400

    ci: update seo and meta tags

--- app.vue
+++ app.vue
@@ -9,67 +9,6 @@ useHead({
       href: '/favicon.ico',
     },
   ],
-  // meta: [
-  //   {
-  //     property: 'og:type',
-  //     content: 'website',
-  //   },
-  //   {
-  //     property: 'og:title',
-  //     content: 'Maikel Carballo',
-  //   },
-  //   {
-  //     property: 'og:url',
-  //     content: 'https://kimael-code.github.io/',
-  //   },
-  //   {
-  //     property: 'og:image',
-  //     content:
-  //       'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
-  //   },
-  //   {
-  //     property: 'og:description',
-  //     content: 'Desarrollador Web Full Stack',
-  //   },
-  //   {
-  //     name: 'twitter:card',
-  //     content: 'summary_large_image',
-  //   },
-  //   {
-  //     name: 'twitter:title',
-  //     content: 'Maikel Carballo',
-  //   },
-  //   {
-  //     name: 'twitter:site',
-  //     content: 'https://kimael-code.github.io/',
-  //   },
-  //   {
-  //     name: 'twitter:creator',
-  //     content: '@_kimael_',
-  //   },
-  //   {
-  //     name: 'twitter:image',
-  //     content:
-  //       'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
-  //   },
-  //   {
-  //     name: 'twitter:description',
-  //     content: 'Desarrollador Web Full Stack',
-  //   },
-  //   {
-  //     name: 'description',
-  //     content: 'Sitio Web Personal de Maikel Carballo',
-  //   },
-  //   {
-  //     name: 'author',
-  //     content: 'Maikel Carballo',
-  //   },
-  //   {
-  //     name: 'keywords',
-  //     content:
-  //       'programador web, desarrollador web, full stack, portafolio, portfolio',
-  //   },
-  // ],
 })
 </script>
 
@@ -77,7 +16,7 @@ useHead({
   <div>
     <Html :lang="$i18n.locale"></Html>
     <NuxtLayout>
-      <NuxtLoadingIndicator />
+      <NuxtLoadingIndicator color="#0047e1" />
       <NuxtPage />
     </NuxtLayout>
   </div>
