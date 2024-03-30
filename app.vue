c6aacc627f4db8598b8d74410ffa65c68e9abef5
Maikel Carballo <mcarballo@tutanota.com>
2024-03-30 19:34:01 -0400

    fix: move seo meta tags definition into `nuxt.config.ts`

--- app.vue
+++ app.vue
@@ -9,24 +9,6 @@ useHead({
     },
   ],
 })
-useServerSeoMeta({
-  name: 'Mi Portafolio',
-  description: 'Este es mi portafolio web.',
-  content: 'Portafolio Web de Maikel Carballo.',
-  author: 'Maikel Carballo',
-  keywords: 'programador web, desarrollador web, full stack, portafolio, portfolio, web developer, developer, full stack web developer',
-  ogType: 'website',
-  ogTitle: 'Maikel Carballo',
-  ogUrl: 'https://kimael-code.github.io/',
-  ogImage: 'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
-  ogDescription: 'Este es mi portafolio web.',
-  twitterCard: 'summary_large_image',
-  twitterTitle: 'Mi Portafolio',
-  twitterSite: 'https://kimael-code.github.io/',
-  twitterCreator: '@_kimael_',
-  twitterImage: 'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
-  twitterDescription: 'Este es mi portafolio web.',
-})
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,69 @@ import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
+    head: {
+      meta: [
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          property: 'og:title',
+          content: 'Mi Portafolio',
+        },
+        {
+          property: 'og:url',
+          content: 'https://kimael-code.github.io/',
+        },
+        {
+          property: 'og:image',
+          content:
+            'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
+        },
+        {
+          property: 'og:description',
+          content: 'Este es mi portafolio web.',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          name: 'twitter:title',
+          content: 'Mi Portafolio',
+        },
+        {
+          name: 'twitter:site',
+          content: 'https://kimael-code.github.io/',
+        },
+        {
+          name: 'twitter:creator',
+          content: '@_kimael_',
+        },
+        {
+          name: 'twitter:image',
+          content:
+            'https://pbs.twimg.com/profile_images/1361041473366601728/mYIfVGNd_400x400.jpg',
+        },
+        {
+          name: 'twitter:description',
+          content: 'Este es mi portafolio web.',
+        },
+        {
+          name: 'description',
+          content: 'Este es mi portafolio web.',
+        },
+        {
+          name: 'author',
+          content: 'Maikel Carballo',
+        },
+        {
+          name: 'keywords',
+          content:
+            'programador web, desarrollador web, full stack, portafolio, portfolio, web developer, developer, full stack web developer',
+        },
+      ],
+    },
     pageTransition: { name: 'page', mode: 'out-in' },
   },
   build: {
